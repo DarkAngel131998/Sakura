c375e42d8258111324ed33d9cfbc0e837f392f3b
lien2512 <liennguyen.uet.vnu@gmail.com>
2019-12-12 11:07:17 +0700

    change

--- src/app/modules/staff/staff-detail/staff-detail.module.ts
+++ src/app/modules/staff/staff-detail/staff-detail.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StaffDetailComponent } from './staff-detail.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
-
+import { ViewStaff } from '../interfaces/viewStaff';
+import { StaffService } from '../staff.service';
 @NgModule({
   imports: [
     CommonModule,
--- src/app/modules/statistic/statistic.component.ts
+++ src/app/modules/statistic/statistic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-statistic',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistic.component.scss']
 })
 export class StatisticComponent implements OnInit {
-
+  chart: Chart;
   constructor() { }
 
   ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+    data: {
+      labels: ['Bug Test', 'Bug UAT', 'Cost', 'CSS score'],
+      datasets: [
+        {
+          data: [
+            project.bugtest, project.buguat, project.pcost, project.cssscore
+          ],
+          backgroundColor: ['#0074D9', '#2ECC40', '#FF4136', '#00ff00']
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Color test'
+      },
+      legend: {
+        position: 'left',
+        display: true,
+        fullWidth: true,
+        labels: {
+          fontSize: 11
+        }
+      },
+      scales: {
+        xAxes: [{
+          display: true
+        }],
+        yAxes: [{
+          display: true
+        }]
+      }
+    }
+    });
   }
 
 }
